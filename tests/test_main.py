47c3ca0dfa75c8427fc5a37f028108d169af4ac9
Angel Antonio Gomera <antoniogomera12@gmail.com>
2024-11-09 00:57:14 -0600

    my first commit

---
+++ tests/test_main.py
@@ -0,0 +1,9 @@
+import unittest
+from main import saludo
+
+class TestSaludo(unittest.TestCase):
+    def test_saludo(self):
+        self.assertEqual(saludo(), "Hola, Mundo")
+
+if __name__ == "__main__":
+    unittest.main()
